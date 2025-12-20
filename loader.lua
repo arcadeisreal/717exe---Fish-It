e83fe59cfe4472cfccd65166cfe35c0bd8247368
arcadeisreal <rioirvansyah5555@gmail.com>
2025-12-20 11:16:43 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/5404709b362eec15f69fc35ad3f09938674b4e79eb6c7bc9dddcf30eb6afc6b3/download"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/308343415cf730b507b461adb1b252ec4eea500a663123c2065204b146a141ac/download"))()
