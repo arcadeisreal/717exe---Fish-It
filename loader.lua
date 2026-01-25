86c9e92868d4b38b56f9f7956d3d5a08e5304445
arcadeisreal <rioirvansyah5555@gmail.com>
2026-01-25 19:49:16 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/294052fb28ea43a9"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/f273f3e08f287a70120392c6678dcd7c62c52eabf3656a812e0994f9aad4081b/download"))()
